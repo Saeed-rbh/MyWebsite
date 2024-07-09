c65439f7875195e25852ecf3d09b583cf265a6d5
Saeed Arabha <saeedarabha@outlook.com>
2024-07-09 12:36:19 -0400

    7-9-2024 12:36

--- InstaCrawler/Website/instagram/src/MoneyMonitor/AddTransactionFeed.jsx
+++ InstaCrawler/Website/instagram/src/MoneyMonitor/AddTransactionFeed.jsx
@@ -1,8 +1,45 @@
-import React, { useState, useEffect, useRef } from "react";
+import { animate } from "framer-motion";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  useMemo,
+  useCallback,
+} from "react";
 import { FaCheck } from "react-icons/fa";
 import { FaXmark } from "react-icons/fa6";
 import { MdOutlineAttachMoney } from "react-icons/md";
 import { useSpring, animated } from "react-spring";
+
+const AmountLogo = ({
+  Animate1,
+  Animate2,
+  Animate3,
+  style1,
+  style2,
+  style3,
+  fontColor,
+}) => {
+  return (
+    <div className="AddTransactionFeed_AmountLogo">
+      {Animate1 && (
+        <animated.div style={style1} className="AddTransactionFeed_Logo">
+          <FaXmark color={fontColor} />
+        </animated.div>
+      )}
+      {Animate2 && (
+        <animated.div style={style2} className="AddTransactionFeed_Logo">
+          <FaCheck color={fontColor} />
+        </animated.div>
+      )}
+      {Animate3 && (
+        <animated.div style={style3} className="AddTransactionFeed_Logo">
+          <MdOutlineAttachMoney color={fontColor} />
+        </animated.div>
+      )}
+    </div>
+  );
+};
 
 function AddTransactionFeed({ isAddClicked }) {
   const DotStyle = {
@@ -25,129 +62,94 @@ function AddTransactionFeed({ isAddClicked }) {
   const [Animate1, setAnimate1] = useState(true);
   const [Animate2, setAnimate2] = useState(false);
   const [Animate3, setAnimate3] = useState(false);
-  const [Imidiate, setImidiate] = useState(false);
+  const Imidiate = useRef(false);
 
-  const AmountLogo = ({ value }) => {
-    const fontSize =
-      value.length > 5 ? "1.3rem" : value.length > 0 ? "0.7rem" : "0.8rem";
+  const fontSize = useMemo(() => {
+    return value.length > 5 ? "1.3rem" : value.length > 0 ? "0.7rem" : "0.8rem";
+  }, [value.length]);
 
-    const fontColor =
-      value.length > 5
-        ? "var(--Bc-2)"
-        : value.length > 0
-        ? "var(--Fc-2)"
-        : "var(--Gc-2)";
+  const fontColor = useMemo(() => {
+    return value.length > 5
+      ? "var(--Bc-2)"
+      : value.length > 0
+      ? "var(--Fc-2)"
+      : "var(--Gc-2)";
+  }, [value.length]);
 
-    useEffect(() => {
-      if (Imidiate) return;
-      if (value.length === 0 && previousValue.current === 2) {
-        setAnimate1(true);
-        setImidiate(true);
-      } else if (value.length === 2 && previousValue.current === 0) {
-        setAnimate1(false);
-      }
+  const handleValueChange = () => {
+    if (value.length === 0 && previousValue.current === 2) {
+      setAnimate1(true);
+      Imidiate.current = true;
+    } else if (value.length === 2 && previousValue.current === 0) {
+      setAnimate1(false);
+    }
 
-      if (
-        (value.length === 2 && previousValue.current === 0) ||
-        (value.length === 5 && previousValue.current === 6)
-      ) {
-        setAnimate2(true);
-        setImidiate(true);
-      } else if (
-        (value.length === 0 && previousValue.current === 2) ||
-        (value.length === 6 && previousValue.current === 5)
-      ) {
-        setAnimate2(false);
-      }
+    if (
+      (value.length === 2 && previousValue.current === 0) ||
+      (value.length === 5 && previousValue.current === 6)
+    ) {
+      setAnimate2(true);
+      Imidiate.current = true;
+    } else if (
+      (value.length === 0 && previousValue.current === 2) ||
+      (value.length === 6 && previousValue.current === 5)
+    ) {
+      setAnimate2(false);
+    }
 
-      if (value.length === 6 && previousValue.current === 5) {
-        setAnimate3(true);
-        setImidiate(true);
-      } else if (value.length === 5 && previousValue.current === 6) {
-        setAnimate3(false);
-      }
-
-      previousValue.current = value.length;
-    }, [value]);
-
-    const style1 = useSpring({
-      from: {
-        opacity: Animate1 ? 0 : 1,
-        transform: Animate1 ? "scale(2)" : "scale(1)",
-        fontSize: fontSize,
-      },
-      to: {
-        opacity: !Animate1 ? 0 : 1,
-        transform: !Animate1 ? "scale(2)" : "scale(1)",
-        fontSize: fontSize,
-      },
-      immediate: !Imidiate,
-      config: { duration: 500 },
-      onRest: () => {
-        if (Imidiate) {
-          setImidiate(false);
-        }
-      },
-    });
-    const style2 = useSpring({
-      from: {
-        opacity: Animate2 ? 0 : 1,
-        transform: Animate2 ? "scale(2)" : "scale(1)",
-        fontSize: fontSize,
-      },
-      to: {
-        opacity: !Animate2 ? 0 : 1,
-        transform: !Animate2 ? "scale(2)" : "scale(1)",
-        fontSize: fontSize,
-      },
-      immediate: !Imidiate,
-      config: { duration: 500 },
-      onRest: () => {
-        if (Imidiate) {
-          setImidiate(false);
-        }
-      },
-    });
-    const style3 = useSpring({
-      from: {
-        opacity: Animate3 ? 0 : 1,
-        transform: Animate3 ? "scale(2)" : "scale(1)",
-        fontSize: fontSize,
-      },
-      to: {
-        opacity: !Animate3 ? 0 : 1,
-        transform: !Animate3 ? "scale(2)" : "scale(1)",
-        fontSize: fontSize,
-      },
-      immediate: !Imidiate,
-      config: { duration: 500 },
-      onRest: () => {
-        if (Imidiate) {
-          setImidiate(false);
-        }
-      },
-    });
-
-    return (
-      <div className="AddTransactionFeed_AmountLogo">
-        {Animate1 && (
-          <animated.div style={style1} className="AddTransactionFeed_Logo">
-            <FaXmark color={fontColor} />
-          </animated.div>
-        )}
-        {Animate2 && (
-          <animated.div style={style2} className="AddTransactionFeed_Logo">
-            <FaCheck color={fontColor} />
-          </animated.div>
-        )}
-        {Animate3 && (
-          <animated.div style={style3} className="AddTransactionFeed_Logo">
-            <MdOutlineAttachMoney color={fontColor} />
-          </animated.div>
-        )}
-      </div>
-    );
+    if (value.length === 6 && previousValue.current === 5) {
+      setAnimate3(true);
+      Imidiate.current = true;
+    } else if (value.length === 5 && previousValue.current === 6) {
+      setAnimate3(false);
+    }
   };
+
+  const handleOnRest = () => {
+    Imidiate.current = false;
+  };
+
+  const cond1 = useMemo(() => {
+    return [Animate1 && Imidiate.current, !Animate1 && Imidiate.current];
+  }, [Animate1]);
+  const cond2 = useMemo(() => {
+    return [Animate1 && Imidiate.current, !Animate1 && Imidiate.current];
+  }, [Animate1]);
+  const cond3 = useMemo(() => {
+    return [Animate1 && Imidiate.current, !Animate1 && Imidiate.current];
+  }, [Animate1]);
+
+  useEffect(() => {
+    if (
+      !Imidiate.current &&
+      (previousValue.current === 2 ||
+        previousValue.current === 0 ||
+        previousValue.current === 5 ||
+        previousValue.current === 6)
+    ) {
+      handleValueChange();
+    }
+    previousValue.current = value.length;
+  }, [handleValueChange, value.length]);
+
+  const springProps = (animate, Imidiate) => ({
+    from: {
+      opacity: animate ? 0 : 1,
+      transform: animate ? "scale(1.5)" : "scale(1)",
+      fontSize: fontSize,
+    },
+    to: {
+      opacity: !animate ? 0 : 1,
+      transform: !animate ? "scale(1.5)" : "scale(1)",
+      fontSize: fontSize,
+    },
+    config: { duration: 500 },
+    onRest: animate && Imidiate ? handleOnRest : undefined,
+  });
+
+  const style1 = useSpring(springProps(Animate1, Imidiate.current));
+  const style2 = useSpring(springProps(Animate2, Imidiate.current));
+  const style3 = useSpring(springProps(Animate3, Imidiate.current));
 
   return (
     <div className="AddTransactionFeed">
@@ -166,7 +168,16 @@ function AddTransactionFeed({ isAddClicked }) {
             onChange={handleChange}
           />
         </li>
-        <AmountLogo value={value} />
+        <AmountLogo
+          Animate1={Animate1}
+          Animate2={Animate2}
+          Animate3={Animate3}
+          style1={style1}
+          style2={style2}
+          style3={style3}
+          fontColor={fontColor}
+        />
+
         <li>
           <label>Reason:</label>
           <textarea
