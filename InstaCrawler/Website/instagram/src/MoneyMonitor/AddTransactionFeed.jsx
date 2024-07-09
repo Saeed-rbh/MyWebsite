466e2924682afd3606ebdbaa16ff830e8ab23e5e
Saeed Arabha <saeedarabha@outlook.com>
2024-07-09 13:37:02 -0400

    7-9-2024 13:36

--- InstaCrawler/Website/instagram/src/MoneyMonitor/AddTransactionFeed.jsx
+++ InstaCrawler/Website/instagram/src/MoneyMonitor/AddTransactionFeed.jsx
@@ -1,4 +1,3 @@
-import { animate } from "framer-motion";
 import React, {
   useState,
   useEffect,
@@ -8,18 +7,9 @@ import React, {
 } from "react";
 import { FaCheck } from "react-icons/fa";
 import { FaXmark } from "react-icons/fa6";
-import { MdOutlineAttachMoney } from "react-icons/md";
 import { useSpring, animated } from "react-spring";
 
-const AmountLogo = ({
-  Animate1,
-  Animate2,
-  Animate3,
-  style1,
-  style2,
-  style3,
-  fontColor,
-}) => {
+const AmountLogo = ({ Animate1, Animate2, style1, style2, fontColor }) => {
   return (
     <div className="AddTransactionFeed_AmountLogo">
       {Animate1 && (
@@ -30,11 +20,6 @@ const AmountLogo = ({
       {Animate2 && (
         <animated.div style={style2} className="AddTransactionFeed_Logo">
           <FaCheck color={fontColor} />
-        </animated.div>
-      )}
-      {Animate3 && (
-        <animated.div style={style3} className="AddTransactionFeed_Logo">
-          <MdOutlineAttachMoney color={fontColor} />
         </animated.div>
       )}
     </div>
@@ -57,23 +42,21 @@ function AddTransactionFeed({ isAddClicked }) {
     const newValue = event.target.value.replace(/[^0-9]/g, "");
     setValue(newValue.length > 0 ? `$${newValue}` : "");
   };
+  useEffect(() => {
+    setValue("");
+  }, [isAddClicked]);
 
   const previousValue = useRef(value.length);
   const [Animate1, setAnimate1] = useState(true);
   const [Animate2, setAnimate2] = useState(false);
-  const [Animate3, setAnimate3] = useState(false);
   const Imidiate = useRef(false);
 
   const fontSize = useMemo(() => {
-    return value.length > 5 ? "1.3rem" : value.length > 0 ? "0.7rem" : "0.8rem";
+    return value.length > 0 ? "0.9rem" : "1.1rem";
   }, [value.length]);
 
   const fontColor = useMemo(() => {
-    return value.length > 5
-      ? "var(--Bc-2)"
-      : value.length > 0
-      ? "var(--Fc-2)"
-      : "var(--Gc-2)";
+    return value.length > 0 ? "var(--Fc-2)" : "var(--Gc-2)";
   }, [value.length]);
 
   const handleValueChange = useCallback(() => {
@@ -96,13 +79,6 @@ function AddTransactionFeed({ isAddClicked }) {
     ) {
       setAnimate2(false);
     }
-
-    if (value.length === 6 && previousValue.current === 5) {
-      setAnimate3(true);
-      Imidiate.current = true;
-    } else if (value.length === 5 && previousValue.current === 6) {
-      setAnimate3(false);
-    }
   }, [value.length]);
 
   const handleOnRest = () => {
@@ -112,10 +88,7 @@ function AddTransactionFeed({ isAddClicked }) {
   useEffect(() => {
     if (
       !Imidiate.current &&
-      (previousValue.current === 2 ||
-        previousValue.current === 0 ||
-        previousValue.current === 5 ||
-        previousValue.current === 6)
+      (previousValue.current === 2 || previousValue.current === 0)
     ) {
       handleValueChange();
     }
@@ -124,22 +97,21 @@ function AddTransactionFeed({ isAddClicked }) {
 
   const springProps = (animate, Imidiate) => ({
     from: {
-      opacity: animate ? 0 : 1,
-      transform: animate ? "scale(2)" : "scale(1)",
+      opacity: animate ? 0 : 0.4,
+      transform: animate ? "scale(1.5)" : "scale(1)",
       fontSize: fontSize,
     },
     to: {
-      opacity: !animate ? 0 : 1,
-      transform: !animate ? "scale(2)" : "scale(1)",
+      opacity: !animate ? 0 : 0.4,
+      transform: !animate ? "scale(1.5)" : "scale(1)",
       fontSize: fontSize,
     },
-    config: { duration: 300 },
+    config: { duration: 500 },
     onRest: animate && Imidiate ? handleOnRest : undefined,
   });
 
   const style1 = useSpring(springProps(Animate1, Imidiate.current));
   const style2 = useSpring(springProps(Animate2, Imidiate.current));
-  const style3 = useSpring(springProps(Animate3, Imidiate.current));
 
   return (
     <div className="AddTransactionFeed">
@@ -162,15 +134,21 @@ function AddTransactionFeed({ isAddClicked }) {
         <AmountLogo
           Animate1={Animate1}
           Animate2={Animate2}
-          Animate3={Animate3}
           style1={style1}
           style2={style2}
-          style3={style3}
           fontColor={fontColor}
         />
 
         <li>
           <label>Reason:</label>
+          <textarea
+            type="text"
+            maxlength="75"
+            placeholder="Shopping for party"
+          />
+        </li>
+        <li>
+          <label>Time & Date:</label>
           <textarea
             type="text"
             maxlength="75"
