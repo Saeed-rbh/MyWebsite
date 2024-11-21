0c6238cb4d84de925e72d017754be0838b67c257
Saeed Arabha <saeedarabha@outlook.com>
2024-11-21 14:17:14 -0500

    11-21-2024 14:17

--- InstaCrawler/Website/instagram/src/Academic/Main/MainTitle.jsx
+++ InstaCrawler/Website/instagram/src/Academic/Main/MainTitle.jsx
@@ -98,7 +98,7 @@ const MainTitle = ({ duration, initialDelay, delayIncrement }) => {
           duration={duration}
         />
       </animated.div>
-      <animated.p style={title2Style}>Academic CV-11</animated.p>
+      <animated.p style={title2Style}>Academic CV-1</animated.p>
     </animated.div>
   );
 };
