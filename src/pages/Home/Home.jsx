4e4fa7bdd8b2ce333435df05a90ec717d4056649
Saeed Arabha <saeedarabha@outlook.com>
2026-05-27 15:13:57 -0400

    Replace water droplet icon with proper fire/flame icon for Heat Transfer popup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -128,7 +128,7 @@ const HomePage = () => {
 
     if (normalizedWord.includes("heat transfer")) {
       setPopupContent({
-        title: <span style={{ display: "flex", alignItems: "center" }}><AnimatedPopupIcon d="M12 2c0 0-6 7.5-6 12a6 6 0 1 0 12 0c0-4.5-6-12-6-12z" /> Heat Transfer</span>,
+        title: <span style={{ display: "flex", alignItems: "center" }}><AnimatedPopupIcon d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.224-4.054 2-6 .5 2.5 2 4.9 4 6.5 2 1.6 3 3.5 3 5.5a7 7 0 1 1-14 0c0-1.153.433-2.294 1-3a2.5 2.5 0 0 0 2.5 2.5z" /> Heat Transfer</span>,
         content: (
           <div style={{ textAlign: "left" }}>
             <div className={`${styles.animateEnter} ${styles.delay1}`}>
